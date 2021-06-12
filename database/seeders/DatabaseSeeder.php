525a48ed89dda8fd8d64912d2eb30a6bd1e0fec8
Ivan Firestone <ivan.firestone@icloud.com>
2021-06-12 16:23:46 -0400

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,28 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\AcademicOffer;
-use App\Models\Announcement;
-use App\Models\AnnouncementDetail;
-use App\Models\Aplication;
-use App\Models\AplicationDetail;
-use App\Models\Province;
-use App\Models\Municipality;
-use App\Models\Campus;
-use App\Models\User;
-use App\Models\Institution;
-use App\Models\Evaluator;
-use App\Models\Offerer;
-use App\Models\Candidate;
-use App\Models\Coordinator;
-use App\Models\EducationLevel;
-use App\Models\Evaluation;
-use App\Models\EvaluationAnswer;
-use App\Models\EvaluationRequirement;
-use App\Models\EvaluationResult;
-use App\Models\Scholarship;
-use App\Models\ScholarshipDetail;
-use App\Models\InstitutionOffer;
 
 class DatabaseSeeder extends Seeder
 {
